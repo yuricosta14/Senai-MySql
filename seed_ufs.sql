507a234e72a60b483beac7209b9040e8ed0bf583
Yuri Costa <110433538+yuricosta14@users.noreply.github.com>
2022-08-18 21:53:02 -0300

    seed_ufs.sql
    Script para inserir todos os estados no banco de dados, atividade realizada no SENAI

---
+++ seed_ufs.sql
@@ -0,0 +1,61 @@
+INSERT INTO uf (sigla, nome, codigo_ibge)
+VALUES  
+('AC', 'ACRE',12),
+('AL', 'ALAGOAS',27),
+('AM', 'AMAZONAS',13),
+('AP', 'AMAPÁ',16),
+('BA', 'BAHIA',29),
+('CE', 'CEARÁ',23),
+('DF', 'DISTRITO FEDERAL',53),
+('ES', 'ESPIRITO SANTO',32),
+('GO', 'GOIÁS',52),
+('MA', 'MARANHÃO',21),
+('MG', 'MINAS GERAIS',31),
+('MS', 'MATO GROSSO DO SUL',50),
+('MT', 'MATO GROSSO',51),
+('PA', 'PARÁ',15),
+('PB', 'PARAÍBA',25),
+('PE', 'PERNAMBUCO',26),
+('PI', 'PIAUÍ',22),
+('PR', 'PARANÁ',41),
+('RJ', 'RIO DE JANEIRO',33),
+('RN', 'RIO GRANDE DO NORTE',24),
+('RO', 'RONDÔNIA',11),
+('RR', 'RORAIMA',14),
+('RS', 'RIO GRANDE DO SUL',43),
+('SC', 'SANTA CATARINA',42),
+('SE', 'SERGIPE',28),
+('SP', 'SÃO PAULO',35),
+('TO', 'TOCANTINS',17)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
